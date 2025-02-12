c0b40da93ac24e0b72574984f919947e7c3aeb05
sosag <sosag@GAEL>
2025-02-12 16:49:21 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace puntuacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("inserte la puntuacion");
+            int p=int.Parse(Console.ReadLine());
+            if (p >= 100 )
+
+            {
+                System.Console.WriteLine("Tu puntuacion es alta");
+            }
+            else
+            {
+                System.Console.WriteLine("Tu puntuacion es menor");
+            }
+             Console.ReadKey();
+        }
+    }
+}
